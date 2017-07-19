eb2574c993025eafac84e3beec92c235594758e0
kukhterin <yury.kukhterin@hdsoft.ru>
2017-07-19 17:42:31 +0700

    fixed reverse

--- Arrays/reversit/reverseIt.cpp
+++ Arrays/reversit/reverseIt.cpp
@@ -7,11 +7,11 @@
 void reverseit(char s[])
 {
   size_t len = strlen(s);
-  for(size_t i=0; i < len/2; i++)
+  for(size_t i = 0; i < len / 2; i ++)
   {
     char temp = s[i];
-    s[i] = s[len-i-1];
-    s[len-i-1] = temp;
+    s[i] = s[len - i - 1];
+    s[len-i - 1] = temp;
 
   }
 }
--- Arrays/reversit/test.cpp
+++ Arrays/reversit/test.cpp
@@ -6,14 +6,14 @@
 void test(const char* s1, const char* s2)
 {
   
-  char* c1 = new char[strlen(s1) +1];
-  strncpy(c1, s1, strlen(s1)+1);
+  char* c1 = new char[strlen(s1) + 1];
+  strcpy(c1, s1);
   
   reverseit(c1);
 
   if(!strcmp(c1,s2))
-  {std::cout << "\nТест пройден!" << std::endl;}
+    {std::cout << "\nТест пройден!" << std::endl;}
   else
-  {std::cout << "Что-то пошло не так!" <<std::endl;}
-  delete c1;
+    {std::cout << "Что-то пошло не так!" <<std::endl;}
+  delete[] c1;
 }
